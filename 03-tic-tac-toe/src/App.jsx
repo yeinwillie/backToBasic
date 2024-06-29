86a7a3ddc358b429060e39448930966f8d0c7a47
Yein E <yeinneg@gmail.com>
2024-06-29 00:25:38 -0300

    ticWinner

--- 03-tic-tac-toe/src/App.jsx
+++ 03-tic-tac-toe/src/App.jsx
@@ -21,7 +21,17 @@ const Square = ({children, isSelected, updateBoard, index}) => {
   )
 }
 
+const WINNER_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
 
+]
 
 
 function App() {
@@ -30,11 +40,27 @@ const [board, setBoard] = useState(Array(9).fill(null))
 
 const [turn, setTurn] = useState(TURNS.X)
 
+// null es que no hay ganador y false es que hay un empate
+const [ winner , setWinner] = useState(null)
 
+const checkWinner = (boardToCheck) => {
+  for (const combo of WINNER_COMBOS){
+    const [a, b, c] = combo
+    if (
+      boardToCheck[a] &&
+      boardToCheck[a] === boardToCheck[b] &&
+      boardToCheck[a]  === boardToCheck[c]  
+
+    ) {
+      return boardToCheck[a] 
+    }
+  }
+   return null
+}
 
 const updateBoard = (index) => {
   // evitar que se actualice la posicion si ya esta llena
-  if (board[index]) return 
+  if (board[index] || winner) return 
   // actualizar el tablero
   const newBoard = [...board]  // spread para no mutar el estado y afectar el renderizado
   newBoard[index] = turn
@@ -42,6 +68,13 @@ const updateBoard = (index) => {
   // cambiar el turno al hacer click en un cuadro
   const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
   setTurn(newTurn)
+
+  // revisar si hay un ganador
+  const newWinner = checkWinner(newBoard)
+  if (newWinner){
+    setWinner(newWinner)
+
+  }
 
 }
 
